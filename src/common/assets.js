3ef8693ecd887d194b7ac9dd6f9e84e9752a7237
Tatsiana <badpanda@tut.by>
2024-11-23 13:00:38 +0300

    Ship is flying, bullets are rendered and moving.

--- src/common/assets.js
+++ src/common/assets.js
@@ -2,21 +2,22 @@ import {Assets} from 'pixi.js';
 import appTextures, { allTextureKeys } from './textures';
 
 Object.entries(appTextures).forEach(([key, value]) => {
-    console.log("assets.js" + key +" "+ value);
+   // console.log("assets.js" + key +" "+ value);
     Assets.add({ alias: key, src: value});
-    console.log("assets end");
+   // console.log("assets end");
 })
 
 const textures = new Map()
 
-export const loadAssets = (onProgress) => {
+export const loadAssets = async (onProgress) => {
     console.log("loadAssets");
     const keys = Object.entries(allTextureKeys).map(([key, value]) => value)
-    Assets.load([...keys], onProgress).then((data) => {
+    await Assets.load([...keys], onProgress).then((data) => {
         console.log("loading...  "+data);
         Object.entries(data).forEach(([key, value]) => {
             textures.set(key, value)
         })
+        console.log("end loadAssets");
         onProgress('all')
     })
 }
--- src/game.js
+++ src/game.js
@@ -1,85 +1,76 @@
 import * as PIXI from 'pixi.js'
 import { loadAssets } from './common/assets'
+import 'constants'
+import appConstants from './common/constants.JS'
+import { addPlayer, getPlayer, playerShoots, playerTick } from './sprite/player';
+import { initBullets, bulletTick } from './sprite/bullets';
 
-const createScene = () => {
-    console.log("create scene");
+const WIDTH = appConstants.size.WIDTH;
+const HEIGHT = appConstants.size.HEIGHT;
+
+const gameState = {
+    stopped: false,
+    moveLeftActive: false,
+    moveRightActive: false
 }
 
-const initInteraction = () => {}
+const createScene = async () => {
+    console.log("createScene");
+    const app = new PIXI.Application();
+    await app.init({
+        background: '#000000',
+        antialias: true,
+        width: WIDTH,
+        height: HEIGHT
+    });
+
+    // Append the application canvas to the document body
+    document.body.appendChild(app.canvas);
+    gameState.app = app;
+
+    const rootContainer = app.stage;
+    rootContainer.interactive = true;
+    rootContainer.hitArea = app.screen;
+   
+    const bullets = initBullets(app, rootContainer);
+    rootContainer.addChild(bullets);
+
+    const player = addPlayer(app, rootContainer);
+    rootContainer.addChild(player);
+
+    console.log("end create scene");
+    return app;
+}
+
+const initInteraction = () => {
+    let player = getPlayer();
+    console.log(player);
+    gameState.mousePosition = player.position.x;
+
+    gameState.app.stage.addEventListener("pointermove", (e) => {
+        gameState.mousePosition = e.global.x;
+    })
+
+    document.addEventListener("keydown", (e) => {
+        if(e.code === 'Space'){
+            playerShoots();
+        }
+    })
+
+    gameState.app.ticker.add((delat) => {
+        playerTick(gameState);
+        bulletTick();
+    })
+}
 
 export const initGame = () => {
     console.log("initGame");
   
-    loadAssets((progress) => {
+    loadAssets(async (progress) => {
       if(progress === 'all'){
-         createScene()
-         initInteraction()
+         await createScene();
+         initInteraction();
       }
     })
  
  }
-
-
- /*
- import { Application, Assets, Sprite  } from 'pixi.js';
- import appTextures, { allTextureKeys } from './common/textures';
-
-// Asynchronous IIFE
-export const initGame = () => {
-(async () =>
-{
-    // Create a PixiJS application.
-    const app = new Application();
-    console.log("new Application();");
-    // Intialize the application.
-    await app.init({ background: '#1099bb', resizeTo: window });
-
-    // Then adding the application's canvas to the DOM body.
-    document.body.appendChild(app.canvas);
-
-     // Load the bunny texture.
-   /*  for (const [key, value] of Object.entries(appTextures)) {
-        console.log(`${key}: ${value}`);
-        Assets.add(key, value);
-        console.log("assets end");
-      }*/
-  /*   const textures = new Map();
-     const keys = Object.entries(allTextureKeys).map(([key, value]) => value)
-     let arrayForAsset = [];
-     Object.entries(appTextures).forEach(([key, value]) => {
-        textures.set(key, value)
-    })
-
-    const textures2 = await Assets.load(textures);
-     const texture = await Assets.load('assets/sprites/enemy/shipBlue.png');
-     const textures1 = await Assets.load([
-        {
-            alias: 'shipBlue',
-            src: 'assets/sprites/enemy/shipBlue.png',
-        },
-        {
-            alias: 'shipBlue2',
-            src: 'assets/sprites/enemy/shipBlue2.png',
-        }])
-     // Create a new Sprite from an image path
-     const bunny = new Sprite(textures2['shipBlue']);
- 
-     // Add to stage
-     app.stage.addChild(bunny);
- 
-     // Center the sprite's anchor point
-     bunny.anchor.set(0.5);
- 
-     // Move the sprite to the center of the screen
-     bunny.x = app.screen.width / 2;
-     bunny.y = app.screen.height / 2;
-
-
-    // Add an animation loop callback to the application's ticker.
-    app.ticker.add((time) =>
-        {
-           
-            bunny.rotation += 0.1 * time.deltaTime;
-        });
-})();
-} */
